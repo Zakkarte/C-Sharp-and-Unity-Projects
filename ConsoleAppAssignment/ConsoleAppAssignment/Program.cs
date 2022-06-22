0ed33379ee7695679fdf36529aec0f1884eec39e
Zakkarte <dakotabkent@gmail.com>
2022-06-22 10:13:19 -0400

    as per instructions

--- ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -8,8 +8,45 @@ namespace ConsoleAppAssignment
         static void Main()
         {
             Console.WriteLine("Guess a number:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed1 = number1 == 12;
+
+
+            while (!isGuessed1)
+            {
+                switch (number1)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12, That is correct.");
+                        isGuessed1 = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess a number?");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+
+            Console.WriteLine("Guess a number:");
             int number = Convert.ToInt32(Console.ReadLine());
             bool isGuessed = number == 12;
+
 
             do
             {
