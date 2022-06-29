251be869ee51fbd6e31a09fc9ade202bad97e145
Zakkarte <dakotabkent@gmail.com>
2022-06-29 13:19:46 -0400

    as per instructions

---
+++ ConsoleAppMethodsAssignment/ConsoleAppMethodsAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            var userValue = Convert.ToInt32(Console.ReadLine());
+
+            var mathsObject = new maths();
+            var resultAdd = mathsObject.Add(userValue, 50);
+            var resultSub = mathsObject.Subtract(userValue, 10);
+            var resultDivide = mathsObject.Divide(userValue, 2);
+
+            Console.WriteLine("-------------");
+            Console.WriteLine(userValue + " + 50 = " + resultAdd);
+            Console.WriteLine("-------------");
+            Console.WriteLine(userValue + " - 10 = " + resultSub);
+            Console.WriteLine("-------------");
+            Console.WriteLine(userValue + " / 2 = " + resultDivide);
+            Console.WriteLine("-------------");
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleAppMethodsAssignment/ConsoleAppMethodsAssignment/maths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethodsAssignment
+{
+    
+    public class maths
+    {
+        
+        public int Add(int data, int value)
+        {
+            return data + value;
+        }
+
+        public int Subtract(int data, int value)
+        {
+            return data - value;
+        }
+
+        public float Divide(float data, float value)
+        {
+            return data / value;
+        }
+    }
+}
