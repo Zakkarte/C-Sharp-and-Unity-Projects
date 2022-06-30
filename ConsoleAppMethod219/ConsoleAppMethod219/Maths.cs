760dc4ff7d6ccabf4ad976961353c4c7c3cc10ac
Zakkarte <dakotabkent@gmail.com>
2022-06-30 14:27:05 -0400

    as per instructions

---
+++ ConsoleAppMethod219/ConsoleAppMethod219/Maths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethod219
+{
+    public class Maths
+    {
+        public int Add(int a, int b = 10)
+        {
+            return a + b;
+           
+        }
+    }
+}
---
+++ ConsoleAppMethod219/ConsoleAppMethod219/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethod219
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+            var mathsObject = new Maths();
+            Console.WriteLine("Please enter a number:");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Please enter a second number or leave blank for default 10");
+            var userResponse=Console.ReadLine();
+
+            int result;
+
+            if (userResponse == "")
+            {
+                 result = mathsObject.Add(userValue);
+                Console.WriteLine("You are using the default value");
+            }
+            else
+            {
+                 result = mathsObject.Add(userValue, Convert.ToInt32(userResponse));
+            }
+
+            Console.WriteLine(userValue + userResponse + " + " + result);
+
+            Console.Read();
+
+
+
+
+
+
+        }
+
+    }
+}
