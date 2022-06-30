ca1c6443200079031f9ca33c6cc8257c1dce5b41
Zakkarte <dakotabkent@gmail.com>
2022-06-30 12:51:40 -0400

    as per instructions

---
+++ ConsoleAppMethodAssignment/ConsoleAppMethodAssignment/Maths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethodAssignment
+{
+    public class Maths
+    {
+        public int Add(int data, int value)
+        {
+            return data + value;
+        }
+
+        public int decSub(decimal data, int value)
+        {
+            return (Convert.ToInt32(data)) - value;
+        }
+
+        public string stringCon(string data, int value)
+        {
+            return data + value;
+        }
+    }
+}
---
+++ ConsoleAppMethodAssignment/ConsoleAppMethodAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an number");
+            var userValue = Convert.ToInt32(Console.ReadLine());
+
+            var mathsObject = new Maths();
+            int resultAdd = mathsObject.Add(userValue, 50);
+            Console.WriteLine(userValue + " + 50 = " + resultAdd);
+            Console.WriteLine("-------------");
+            Console.WriteLine("Please input a number as decimal");
+            var decUserValue = Convert.ToDecimal(Console.ReadLine());
+
+            int resultDecSub = mathsObject.decSub(decUserValue, 10);
+            Console.WriteLine(decUserValue + " - 10 = " + resultDecSub);
+            Console.WriteLine("-------------");
+
+            Console.WriteLine("Please input a string of text that will be added to an int");
+            string stringValue = (Console.ReadLine());
+
+            var restulString = mathsObject.stringCon(stringValue, 200);
+            Console.WriteLine(stringValue + " + 200 :" + restulString);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
