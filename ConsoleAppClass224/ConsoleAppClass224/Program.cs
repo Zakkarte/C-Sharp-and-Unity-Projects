97c3d3829cdcd082e45333bf939eb3570651d7af
Zakkarte <dakotabkent@gmail.com>
2022-07-06 12:00:59 -0400

    as per instructions

---
+++ ConsoleAppClass224/ConsoleAppClass224/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClass224
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathsObject = new maths(); //instantiate
+            Console.WriteLine("Please enter a number:");
+            var userInput = Console.ReadLine();
+            int userResult;
+            mathsObject.Halve(int.Parse(userInput), out userResult);
+
+            Console.WriteLine(userInput + " halved is : " + userResult);
+
+            Console.Read();
+            
+        }
+    }
+}
---
+++ ConsoleAppClass224/ConsoleAppClass224/maths.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClass224
+{
+    class maths
+    {
+        public void Halve(int a, out int result)
+        {
+            result = a / 2;
+        }
+        
+    }
+}
