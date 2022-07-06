91173407c585acfde0f9203daca1b1d94a7cb410
Zakkarte <dakotabkent@gmail.com>
2022-07-06 11:15:30 -0400

    as per instructions

---
+++ ConsoleMethod220/ConsoleMethod220/Maths.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMethod220
+{
+    public class Maths
+    {
+        public void Method(int a, int b)
+        {
+            int returnvalue = (a + 200);
+            Console.WriteLine(a + " + 200: " + returnvalue);
+            Console.WriteLine(b);
+        }
+
+
+    }
+}
---
+++ ConsoleMethod220/ConsoleMethod220/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMethod220
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            var mathsobject = new Maths();
+            mathsobject.Method(100, 200);
+
+            Console.Read();
+
+        }
+    }
+}
