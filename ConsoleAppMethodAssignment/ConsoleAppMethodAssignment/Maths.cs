5465e5d342cb3e1714bbea76b1f50b9e5e47e1d4
Zakkarte <dakotabkent@gmail.com>
2022-07-06 10:41:00 -0400

    as per instructions

--- ConsoleAppMethodAssignment/ConsoleAppMethodAssignment/Maths.cs
+++ ConsoleAppMethodAssignment/ConsoleAppMethodAssignment/Maths.cs
@@ -8,17 +8,17 @@ namespace ConsoleAppMethodAssignment
 {
     public class Maths
     {
-        public int Add(int data, int value)
+        public int Method(int data, int value)
         {
             return data + value;
         }
 
-        public int decSub(decimal data, int value)
+        public int Method(decimal data, int value)
         {
             return (Convert.ToInt32(data)) - value;
         }
 
-        public string stringCon(string data, int value)
+        public string Method(string data, int value)
         {
             return data + value;
         }
