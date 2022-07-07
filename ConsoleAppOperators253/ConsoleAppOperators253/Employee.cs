08380e773f0a3378b4b8fdb20b1dd09e3666d557
Zakkarte <dakotabkent@gmail.com>
2022-07-07 14:44:08 -0400

    as per instructions

---
+++ ConsoleAppOperators253/ConsoleAppOperators253/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOperators253
+{
+    public class Employee
+    {
+        public int _EmployeeID { get; set; }
+        public string _EmployeeName { get; set; }
+
+        public Employee(int EmployeeID, string EmployeeName)
+        {
+            _EmployeeID = EmployeeID;
+            _EmployeeName = EmployeeName;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var employee1 = (Employee)obj;
+            return (_EmployeeID == employee1._EmployeeID) && (_EmployeeName == employee1._EmployeeName);
+
+        }
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+
+        public static bool operator == (Employee a, Employee b)
+        {
+            if (a.Equals(b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        public static bool operator !=(Employee a, Employee b)
+        {
+            if (a.Equals(b))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleAppOperators253/ConsoleAppOperators253/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOperators253
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Jesse = new Employee(1, "Jesse");
+            var Joe = new Employee(2, "Joe");
+            var JesseDup = new Employee(1, "JesseDup");
+
+            Console.WriteLine(Jesse._EmployeeName + " and " + Joe._EmployeeName + " share the same ID: " + Jesse._EmployeeID.Equals(Joe._EmployeeID));
+            Console.WriteLine(Jesse._EmployeeName + " and " + Joe._EmployeeName + " share the same ID: " + (Jesse._EmployeeID == Joe._EmployeeID));
+            Console.WriteLine(Jesse._EmployeeName + " and " + JesseDup._EmployeeName + " Share the same ID: " + Jesse._EmployeeID.Equals(JesseDup._EmployeeID) + "\n" + "But do they share the same name: " + Jesse._EmployeeName.Equals(JesseDup._EmployeeName));
+
+
+            Console.ReadLine();
+        }
+    }
+}
