349d4fc1339236066147cb6ecf1d1817e4681999
Zakkarte <dakotabkent@gmail.com>
2022-07-12 12:18:01 -0400

    as per instructions

---
+++ ConsoleAppDateTime307/ConsoleAppDateTime307/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleAppDateTime307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time1 = DateTime.Now;
+            Console.WriteLine(time1);
+            
+            Console.WriteLine("Please type out a number:");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            DateTime time2 = time1.AddHours(answer);
+            Console.WriteLine(time2);
+            Console.Read();
+        }
+    }
+}
